3dc5c71cf4c223063f0dc8acc88e8833dde8ac47
Sakshi Suresh Chavan <sakshic2403@gmail.com>
2026-06-28 06:54:07 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountNumber = 12321;
+let accountPassword = "2121";
+var accountEmail = "Sakshi@gmail.com";
+accountName = "sakshi";
+
+//accountNumber = 333;
+accountPassword = "0000";
+accountEmail = "rohan@gmail.com"
+
+console.table([accountNumber,accountPassword,accountEmail,accountName]);
