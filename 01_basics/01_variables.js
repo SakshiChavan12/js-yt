c3288513bb4acf74be3f402757eefa4a0ac0585d
Sakshi Suresh Chavan <sakshic2403@gmail.com>
2026-06-28 07:25:18 +0000

    basic datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,9 +2,16 @@ const accountNumber = 12321;
 let accountPassword = "2121";
 var accountEmail = "Sakshi@gmail.com";
 accountName = "sakshi";
+let accountState;
 
 //accountNumber = 333;
 accountPassword = "0000";
 accountEmail = "rohan@gmail.com"
 
-console.table([accountNumber,accountPassword,accountEmail,accountName]);
+/*
+var is not prefered
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountNumber,accountPassword,accountEmail,accountName,accountState]);
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as a newer version
+
+//alert (3+3) //we are not using browser we are using nodejs
+
+console.log("Sakshi");
+//console.log(3+3);
+
+/*for documentation use mdn web docs by mozilla org 
+but original is ECMAScript doumentation by tc39*/
+
+/*
+Pre-defined datatypes:
+number => range 2 to the power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => not defined 
+symbol => unique
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
+/*
+type of "null" datatype is object 
+and of "undefined" is undefined
+which are unique and interview based questions
+*/
